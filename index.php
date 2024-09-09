6714436c904b4a1d6d78c301cefea9eec7bc2e76
mariannacoppa <densimetra@gmail.com>
2024-09-09 19:52:39 +0200

    [UPD] written functions

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
+function checkParameter($param){
+  // se i caratteri non sono numerici, compare avviso 
+  if(!is_numeric($_GET['length'])){
+    return 'Devi inserire un valore numerico';
+  }
+  // se il numero inserito è inferiore a 6, compare messaggio 
+  if($_GET['length'] < 6){
+    return 'Devi inserire un numero maggiore o uguale a 6';
+  }
+  if(is_numeric($_GET['length']) && $_GET['length'] >= 6){
+    return true;
+  }
+}
+
+// verifico che sia stata compiata la form
+if(isset($_GET['length']) && $_GET['length'] != ''){
+  // richiamo la funzione e assegno il valore restituito alla variabile check
+  $check = checkParameter($_GET['length']);
+  // se il valore è true restituisco la password
+  if($check === true){
+
+  }
+  //altrimenti assegno il valore restituito in una variabile correlata a messaggio
+  else{
+    $message = $check;
+  }
+}
 // imposto la condizione per cui i caratteri devono essere diversi da null, da stringa vuota e numerici
   if(isset($_GET['length']) && is_numeric($_GET['length']) && $_GET['length'] != ''){
     // se i caratteri non sono numerici, compare avviso 
@@ -9,22 +36,21 @@
     if($_GET['length'] < 6){
       $message = 'Devi inserire un numero maggiore o uguale a 6';
     }
-  // creo la stringa contenente i caratteri utilizzabili per la password
-  $baseString = 'abcdefghijklmnopqrstuvwxyz'.strtoupper('abcdefghijklmnopqrstuvwxyz').'0123456789!?&%$<>^+-*/()[]{}@#_=';
-  // recupero un carattere randomico dalla variabile contentente i caratteri utilizzabili
-  $slices = str_split($baseString);
-  $randomChar = rand(0, count($slices) - 1);
-  
-  // definisco una stringa vuota atta a contenere i caratteri della passsword
-  $password = '';
-  // creo un ciclo per appendere il carattere random alla stringa vuota
+    // creo la stringa contenente i caratteri utilizzabili per la password
+    $baseString = 'abcdefghijklmnopqrstuvwxyz'.strtoupper('abcdefghijklmnopqrstuvwxyz').'0123456789!?&%$<>^+-*/()[]{}@#_=';
+    // recupero un carattere randomico dalla variabile contentente i caratteri utilizzabili
+    $slices = str_split($baseString);
+    $randomChar = rand(0, count($slices) - 1);
+    
+    // definisco una stringa vuota atta a contenere i caratteri della passsword
+    $password = '';
+    // creo un ciclo per appendere il carattere random alla stringa vuota
     while(strlen($password) < $_GET['length']){
       // creo un numero random compreso tra 0 e la lunghezza della stringa
       $randomChar = rand(0, count($slices) - 1);
       // appendo il numero alla stringa vuota
       $password .= $baseString[$randomChar];
-    }
-    
+    } 
   }
 ?>
 <!DOCTYPE html>
@@ -51,8 +77,9 @@
                 <div class="col-12">
                   <button type="submit" class="btn btn-sm btn-primary text-center mb-5">Crea</button>
                 </div>
+                <!-- stampo la password generata -->
                 <div class="col-12 text-center">
-                  <?php if(isset($message)) { ?>
+                  <?php if(isset($check) && $check !== true) { ?>
                     <h2 class="text-danger"><?php echo $message; ?></h2>
                   <?php } ?>
                   <?php if(isset($password)) { ?>
