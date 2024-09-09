581d51f58172a030e9c6f3db54f205228e1460bb
mariannacoppa <densimetra@gmail.com>
2024-09-09 19:07:23 +0200

    [UPD] cycled random characters depending on user selected number

--- index.php
+++ index.php
@@ -14,7 +14,17 @@
   // recupero un carattere randomico dalla variabile contentente i caratteri utilizzabili
   $slices = str_split($baseString);
   $randomChar = rand(0, count($slices) - 1);
-  var_dump($baseString[$randomChar]);
+  
+  // definisco una stringa vuota atta a contenere i caratteri della passsword
+  $password = '';
+  // creo un ciclo per appendere il carattere random alla stringa vuota
+    while(strlen($password) < $_GET['length']){
+      // creo un numero random compreso tra 0 e la lunghezza della stringa
+      $randomChar = rand(0, count($slices) - 1);
+      // appendo il numero alla stringa vuota
+      $password .= $baseString[$randomChar];
+    }
+    var_dump($password);
 }
 ?>
 <!DOCTYPE html>
