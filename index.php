b5e30d77b0108afd3dc7cdc69e0e9d4f35354b0d
mariannacoppa <densimetra@gmail.com>
2024-09-09 17:54:52 +0200

    [UPD] set input conditions

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+// imposto la condizione per cui i caratteri devono essere diversi da null, da stringa vuota e numerici
+  if(isset($_GET['length']) && is_numeric($_GET['length']) && $_GET['length'] != ''){
+    // se i caratteri non sono numerici, compare avviso 
+    if(!is_numeric($_GET['length'])){
+      $message = 'Devi inserire un valore numerico';
+    }
+    // se il numero inserito è inferiore a 6, compare messaggio 
+    if($_GET['length'] < 6){
+      $message = 'Devi inserire un numero maggiore o uguale a 6';
+    }
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,12 +23,14 @@
     <div class="container mt-5">
       <div class="row">
         <div class="col-12">
-          <div class="form-container bg-secondary d-flex justify-content-center">
+          <div class="form-container bg-dark d-flex justify-content-center">
             <form action="./index.php" method="get">
               <div class="row">
-                <div class="col-12">
+                <div class="col-12 col-md-6">
+                  <h1 class="text-primary text-center my-2">PHP Strong Password Generator</h1>
                   <label for="" class="control-label text-light mt-5">Inserisci il numero di cifre della password</label>
-                  <input type="number" name="length" id="length" class="form-control form-control-sm my-4" placeholder="lunghezza della password">
+                  <!-- se la cifra è < 6, compare avviso -->
+                  <input type="number" min="6" name="length" id="length" class="form-control form-control-sm my-4 col-md-6" placeholder="Lunghezza della password">
                 </div>
                 <div class="col-12">
                   <button type="submit" class="btn btn-sm btn-primary text-center mb-5">Crea</button>
